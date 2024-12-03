7bc1eed517eb1bad6649451c44304aabc8818c8c
Daniel Lopez Garcia <dolopez@itla.edu.do>
2024-12-03 11:07:55 -0400

    integracion de Form Module

--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,22 +1,31 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-crear-generos',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, MatFormFieldModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './crear-generos.component.htm',
   styleUrl: './crear-generos.component.css'
 })
 export class CrearGenerosComponent {
 
-  router = inject(Router);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
+  form = this.formBuilder.group({
+    nombre: ['']
+  });
   guardarCambios() {
     // ... guardar los cambios
-    this.router.navigate(['/generos']);
+    // this.router.navigate(['/generos']);
+    console.log(this.form.value);
   }
-  
+
 }
 //MatButtonModule, MatIconModule,RouterLink
